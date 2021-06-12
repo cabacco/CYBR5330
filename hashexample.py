a67bb18577be03e62bd77fa70f18adc09400165c
cabacco <coba6526@colorado.edu>
2021-06-12 13:50:40 -0600

    Adding file for first time.

---
+++ hashexample.py
@@ -0,0 +1,9 @@
+#Simple python code to generate and display an md5 hash from user input (from Simon)
+import hashlib
+
+word = input("enter the word to hash:")
+word_bytes = str.encode(word)
+md5 = hashlib.md5(word_bytes)
+result = md5.hexdigest()
+result_string = str(result)
+print(result_string)
