f66b60dbf61d691f48299bbcde28ae57709d1785
isaacgoff <71352870+isaacgoff@users.noreply.github.com>
2021-06-16 17:27:36 -0600

    Delete hashexample.py

--- hashexample.py
+++
@@ -1,9 +0,0 @@
-#Simple python code to generate and display an md5 hash from user input (from Isaac)
-import hashlib
-
-word = input("enter the word to hash:")
-word_bytes = str.encode(word)
-md5 = hashlib.md5(word_bytes)
-result = md5.hexdigest()
-result_string = str(result)
-print(result_string)
