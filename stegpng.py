83eb42806aa98c33b304e70c153135c0195d9caa
Janofermp <85805524+Janofermp@users.noreply.github.com>
2021-07-23 18:05:31 -0600

    Update stegpng.py

--- stegpng.py
+++ stegpng.py
@@ -44,25 +44,6 @@ def ChooseMode():
 
         print('\nSorry, that is not a valid choice. Please enter "1" or "2" to select a mode.')
 
-def to_bin(data):
-
-    """Convert `data` to binary format as string"""
-
-    if isinstance(data, str):
-
-        return ''.join([ format(ord(i), "08b") for i in data ])
-
-    elif isinstance(data, bytes) or isinstance(data, np.ndarray):
-
-        return [ format(i, "08b") for i in data ]
-
-    elif isinstance(data, int) or isinstance(data, np.uint8):
-
-        return format(data, "08b")
-
-    else:
-
-        raise TypeError("Type not supported.")
 
 ''''''
 
